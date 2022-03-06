ab78a38693eea8999dc2393a4ef39f0b54a08c6d
Megan Coats <ctsmgn@yahoo.com>
2022-03-06 00:21:20 -0500

    Completed project for deliverable 2

--- Deliverable_two/src/CountHeadsOrTails.java
+++ Deliverable_two/src/CountHeadsOrTails.java
@@ -1,15 +1,44 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CountHeadsOrTails {
+	public static void coinflip(String headsOrTailsGuess, int numberOfFlips) {
+		float correctCount = 0;
+		float percentageCorrect = 1;
+	
+		
+		for (int i = 0; i < numberOfFlips; i++) {
+			int rand = (int)(Math.random() * ((100 - 1) + 1)) + 1 ;//random number from 0 to 100
+			
+			if (rand%2 == 1) {
+				System.out.println("heads");
+				if(headsOrTailsGuess.equals("heads"))
+				{
+					correctCount++;
+				}
+			}
+				else {
+					System.out.println("tails" );
+					if(headsOrTailsGuess.equals("tails"))
+					{
+						correctCount++;
+					}
+				}
+		}
+		percentageCorrect = (correctCount/numberOfFlips) * 100;
+		
+		System.out.println("Your guess " + headsOrTailsGuess + ", came up " + correctCount + " time(s)."
+					+ "\n" + "That's " + percentageCorrect + "%");
+	}
 	
 
 	public static void main(String[] args) {
 		Scanner scrn = new Scanner(System.in);//Create a Scanner Object to take user imput
 		
-		System.out.println("Heads or Tails?");
+		System.out.println("Guess which will have more: heads or tails?");
 		String headsOrTailsGuess = scrn.nextLine().toLowerCase();
 		
-		System.out.println("How many times would you like to flip the coin?");
+		System.out.println("How many times shall we flip a coin?");
 		int numberOfFlips = scrn.nextInt();
 		int correctCount;
 		
@@ -22,6 +51,8 @@ public class CountHeadsOrTails {
 		else {
 			System.out.println("Incorrect input");
 		}
+		
+		coinflip(headsOrTailsGuess, numberOfFlips);
 		
 	}
 
